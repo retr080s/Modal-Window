30eaa7f2a165c367afef704fc75cf3961d3171b0
retr080s <retr080s@protonmail.com>
2022-10-24 17:23:17 +0200

    modal open/close function

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', function () {
+        console.log('Button clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+
+btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
